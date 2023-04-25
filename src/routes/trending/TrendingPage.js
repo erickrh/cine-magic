c347536a531342dbc0a3330cfce0ad8e3b222405
Erick <123erickrh@gmail.com>
2023-04-25 13:16:58 -0500

    Fix navigation bug on trending route

--- src/routes/trending/TrendingPage.js
+++ src/routes/trending/TrendingPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { GenericList } from '../../ui/GenericList';
 import { useLocation } from 'react-router-dom';
+import { useMovieAPI } from '../../hooks/useMovieAPI';
 
 function TrendingPage() {
   const location = useLocation();
+  const { trendingMovies } = useMovieAPI();
 
   let trends;
   if (location.state?.trendingMovies) {
     trends = location.state.trendingMovies;
+  } else {
+    trends = trendingMovies;
   }
 
   return (
